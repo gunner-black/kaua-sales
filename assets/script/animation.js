41662dc2e6baba83cc558302998e4ee645e7d964
gunner-black <kauasalesfeitosa@gmail.com>
2025-11-02 13:18:02 -0300

    update

---
+++ assets/script/animation.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const elementosAnimar = document.querySelectorAll('.animate-while-rolling');
+  const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.2
+  };
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+  elementosAnimar.forEach(elemento => {
+    observer.observe(elemento);
+  });
+});
